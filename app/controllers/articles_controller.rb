6fa0cfe5ba4f4394041c3da2d6f2a658fe8c7920
Alexandru <rammazzoti2000@gmail.com>
2020-04-15 11:57:36 +0200

    add edit and update actions with templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
     redirect_to article_path(@article)
   end
 
   def destroy
-    Article.find(params[:id]).destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
     redirect_to articles_path()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :articles
+  GET 'articles/:id/edit(.:format)', to: 'articles#edit'
 end
