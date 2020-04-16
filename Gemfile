ccaf62f5392b0122d9b897ecaddcaf472e6508f8
Alexandru <rammazzoti2000@gmail.com>
2020-04-16 14:56:21 +0200

    add a few gems

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
+
+gem "paperclip"
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     tags.join(", ")
